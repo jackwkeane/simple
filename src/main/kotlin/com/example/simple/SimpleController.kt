7accffb0a1bd40a1d3cee27fa886815ad0bb48b2
Jack Keane <jkeane@urbn.com>
2022-06-10 15:41:42 -0400

    Implemented path variable testing

--- src/main/kotlin/com/example/simple/SimpleController.kt
+++ src/main/kotlin/com/example/simple/SimpleController.kt
@@ -1,12 +1,19 @@
 package com.example.simple
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 public class SimpleController {
-    @GetMapping("/helloworld")
-    fun helloWorld(): String {
-        return "Changed Text"
+    @GetMapping("/test")
+    fun test(): String {
+        return "This is a test."
     }
+
+    @GetMapping("/pathvariable/{variable}")
+    fun pathVariable(@PathVariable variable: String): String {
+        return "Your path variable: $variable"
+    }
+
 }
